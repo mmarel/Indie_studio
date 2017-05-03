ec33119a33130195cedb2c8d3eadb626afc9d6f7
khsime_m <marwane.khsime@epitech.eu>
2017-05-03 12:35:19 +0200

    Init

--- install/install.sh
+++ install/install.sh
@@ -1,21 +1,63 @@
 #! /bin/sh
 
-# Clean
-rm -rf irrlicht*
-# Download Irrlicht
-wget http://downloads.sourceforge.net/irrlicht/irrlicht-1.8.4.zip -q --show-progress &&
-# Unzip the target
-unzip irrlicht-1.8.4.zip &&
-# Compile and create the library
-make -C irrlicht-1.8.4/source/Irrlicht/ &&
-# Copy Library .a and headers in the project
-rm -rf ../lib/Linux ../lib/MacOSX ../lib/Win32-gcc ../lib/Win32-visualstudio ../lib/Win64-visualStudio &&
-mkdir -p ../lib &&
-cp -r irrlicht-1.8.4/lib/* ../lib/ &&
-rm -rf ../includes/irr &&
-mkdir -p ../includes/irr &&
-cp -r irrlicht-1.8.4/include/* ../includes/irr &&
-# Erase the tarball and the irrlicht folder
-rm -rf irrlicht-1.8.4.zip irrlicht-1.8.4 &&
+############
+# IRRLICHT #
+############
 
-echo "Install Complete"
+while true; do
+    read -p "Do you wish to install irrlicht (y/n) ? :  " yn
+    case $yn in
+
+        [Nn]* ) break;;
+
+        [Yy]* )
+
+        # Clean
+        rm -rf irrlicht*
+        # Download Irrlicht
+        wget http://downloads.sourceforge.net/irrlicht/irrlicht-1.8.4.zip -q --show-progress &&
+        # Unzip the target
+        unzip irrlicht-1.8.4.zip &&
+        # Compile and create the library
+        make -C irrlicht-1.8.4/source/Irrlicht/ &&
+        # Copy Library .a and headers in the project
+        rm -rf ../lib/Linux ../lib/MacOSX ../lib/Win32-gcc ../lib/Win32-visualstudio ../lib/Win64-visualStudio &&
+        mkdir -p ../lib &&
+        cp -r irrlicht-1.8.4/lib/* ../lib/ &&
+        rm -rf ../includes/irr &&
+        mkdir -p ../includes/irr &&
+        cp -r irrlicht-1.8.4/include/* ../includes/irr &&
+        # Erase the tarball and the irrlicht folder
+        rm -rf irrlicht-1.8.4.zip irrlicht-1.8.4 &&
+
+        echo "Install Complete"
+
+    esac
+done
+
+############
+#  OPENGL  #
+############
+
+while true; do
+    read -p "Do you wish to project dependencies (y/n) ?  " yn
+    case $yn in
+
+        [Nn]* ) break;;
+
+        [Yy]* )
+
+        sudo apt-get install    libegl1-mesa \
+                                libgl1-mesa-dev \
+                                libglu1-mesa \
+                                libglu1-mesa-dev \
+                                libsfml-dev \
+                                libgtest-dev \
+                                binutils-dev \
+                                libtool \
+                                lua5.2 \
+                                liblua5.2-dev \
+                                liblua5.2-0 \
+                                graphviz
+    esac
+done
